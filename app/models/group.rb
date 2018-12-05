289e7f817bc01218fa8dc68238c27a1fcbde54c9
akihiro190 <nishikawaakihiro@nishikawaakihironoMacBook-Pro.local>
2018-12-05 14:40:22 +0900

    Edit message_function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,15 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
+  has_many :messages
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が表示されています'
+    else
+      'まだメッセージがありません'
+    end
+
+  end
 end
