630d2d55daa891c4da110856a89a4b2f90b6c366
akihiro190e <nishikawa_ssd@yahoo.co.jp>
2018-12-12 20:01:14 +0900

    Edit index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,17 +3,17 @@ $(function(){
 
   function appendName(user) {
     var html =
-  `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${user.name}</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-  </div>`
-  search_list.append(html);
+    `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    </div>`
+    search_list.append(html);
   }
 
   function appendNoName(name) {
     var html = `<div class="chat-group-user clearfix">
-  <p class="chat-group-user__name">${name}</p>`
-  search_list.append(html);
+      <p class="chat-group-user__name">${name}</p>`
+      search_list.append(html);
   }
 
   function appendUser(user) {
@@ -24,34 +24,33 @@ $(function(){
       <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
     </div>`
     $('.chat-group-users').append(html);
-
   }
 
   // "#user-serch-field”の部分のテキストフィールドがkeyupしたら、テキストフィールドの文字を取得して変数inputに代入する
   $("#user-search-field").on("keyup", function(){
   // formの値を取得するときはval()を使う
     var input = $("#user-search-field").val();
-     $.ajax({
+    $.ajax({
       type: 'GET',
       url: '/users',
       data: { name: input },
       dataType: 'json'
     })
      // jbuilderでarray!を使用しているため、配列型で情報が返ってきて、namesに代入される
-     .done(function(names){
-       $('.user-search-result').find('.chat-group-user').remove();
+    .done(function(names){
+      $('.user-search-result').find('.chat-group-user').remove();
       if (names.length !== 0) {
-          names.forEach(function(name){
-          appendName(name);
+        names.forEach(function(name){
+        appendName(name);
         });
       }
       else {
         appendNoName("一致する名前はありません");
       }
-     })
-     .fail(function(){
+    })
+    .fail(function(){
       alert('ユーザー検索に失敗しました')
-     })
+    })
   });
 
   $(document).on('click', '.user-search-add' , function() {
@@ -60,7 +59,6 @@ $(function(){
     var id = $(this).attr('data-user-id')
     var user = {name: name, id: id}
       appendUser(user);
-
   });
 
   $(document).on('click', '.user-search-remove' , function() {
